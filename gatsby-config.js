107f9b8eb18d12ba3db32a3faf54cc69fee108e3
Kate Mills <kate.mills1124@gmail.com>
2020-05-10 11:42:08 -0700

    Initial config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Eatery`,
+    description: `Generic Restaurant Website built with Gatsby`,
+    author: `kate-mills`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Restaurant Example`,
+        short_name: `restaurant`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
