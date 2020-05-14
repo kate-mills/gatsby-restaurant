ab5058a92e2b5b129c68febcc164929addc60e99
Kate Mills <kate.mills1124@gmail.com>
2020-05-14 13:32:48 -0700

    feat(QuickInfo) added utils(SectionButton)

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -1,12 +1,51 @@
 import React, { Component } from "react"
-import { Section, Title } from "../../utils"
+import { Section, Title, SectionButton } from "../../utils"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import { styles } from "../../utils"
 
 export default class QuickInfo extends Component {
   render() {
     return (
       <Section>
         <Title message="let us tell you" title="our mission" />
+        <QuickInfoInnerWrapper>
+          <p className="text">
+            {" "}
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <Link
+            to="/about/"
+            style={{ textDecoration: "none", display: "contents" }}
+          >
+            <SectionButton style={{ margin: "2rem auto" }}>about</SectionButton>
+          </Link>
+        </QuickInfoInnerWrapper>
       </Section>
     )
   }
 }
+
+const QuickInfoInnerWrapper = styled.div`
+  width: 90%;
+  margin: 2rem auto;
+  .text {
+    line-height: 2rem;
+    color: ${styles.colors.mainGrey};
+    word-spacing: 0.2rem;
+  }
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 992px) {
+    width: 60%;
+  }
+`
