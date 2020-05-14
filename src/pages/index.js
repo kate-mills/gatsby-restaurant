86c9c19e6bf5e1b9eb09b0320f547ba9bb876764
Kate Mills <kate.mills1124@gmail.com>
2020-05-13 17:35:42 -0700

    menu button added to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from "react"
+import { Link } from 'gatsby'
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { HomeHeader, Banner } from "../utils"
+import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomeHeader img={img}>
-      <Banner title="eatery" subtitle="36 blue street - Santa Monica, ca" />
+      <Banner title="eatery" subtitle="36 blue street - Santa Monica, ca">
+        <BannerButton
+        style={{margin: '2rem auto'}}>menu</BannerButton>
+      </Banner>
     </HomeHeader>
   </Layout>
 )
