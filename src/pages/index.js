e0f51c64ede77a352b4582a83d69ec8e9ae2926c
Kate Mills <kate.mills1124@gmail.com>
2020-05-13 17:48:35 -0700

    feat(new Component) HomePageComponents/QuickInfo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import React from "react"
-import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
 
+import QuickInfo from "../components/HomePageComponents/QuickInfo"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomeHeader img={img}>
       <Banner title="eatery" subtitle="36 blue street - Santa Monica, ca">
-        <BannerButton
-        style={{margin: '2rem auto'}}>menu</BannerButton>
+        <BannerButton style={{ margin: "2rem auto" }}>menu</BannerButton>
       </Banner>
     </HomeHeader>
+    <QuickInfo />
   </Layout>
 )
 export default IndexPage
