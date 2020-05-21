f66606604fd5b54e7292bfd0155d118ad7605818
Kate Mills <kate.mills1124@gmail.com>
2020-05-21 16:10:00 -0700

    copied netlify form example

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,17 +11,35 @@ const ContactPage = () => (
       <Banner title="contact us" subtitle={`we'd love to hear from you`} />
     </PageHeader>
     <Section>
-      <div>
-        <form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input name="name" placeholder="Your Name" type="text" />
-          <button> Send</button>
-        </form>
-      </div>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Section>
   </Layout>
 )
