9bd3640533b9ae2c5d839a6532a196bf66090595
Kate Mills <kate.mills1124@gmail.com>
2020-05-21 16:03:38 -0700

    Testing netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { PageHeader, Banner } from "../utils"
+import { PageHeader, Banner, Section } from "../utils"
 import img from "../images/bcg/contactBcg.jpeg"
 
 const ContactPage = () => (
@@ -10,6 +10,19 @@ const ContactPage = () => (
     <PageHeader img={img}>
       <Banner title="contact us" subtitle={`we'd love to hear from you`} />
     </PageHeader>
+    <Section>
+      <div>
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input name="name" placeholder="Your Name" type="text" />
+          <button> Send</button>
+        </form>
+      </div>
+    </Section>
   </Layout>
 )
 export default ContactPage
