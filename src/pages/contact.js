526aa796832c1b6dcd20508f99ca6a60de35b3c3
Kate Mills <kate.mills1124@gmail.com>
2020-05-22 16:22:02 -0700

    Not giving up on this form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,6 +9,17 @@ const ContactPage = () => (
     <SEO title="Contact" />
     <PageHeader img={img}>
       <Banner title="contact us" subtitle={`we'd love to hear from you`} />
+      <form
+        name="contact-us-form"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input name="name" placeholder="Your Name" type="text" />
+        <input name="email" placeholder="name@name.com" type="email" />
+        <textarea name="message" placeholder="Your Message" />
+        <button>Send</button>
+      </form>
     </PageHeader>
   </Layout>
 )
